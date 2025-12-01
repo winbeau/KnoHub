1305d4d79ef461ccf3b617a530b719ff6852b979
winbeau <geneva4869@163.com>
2025-12-01 20:19:40 +0800

    deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   server: {
+    host: true,
+    allowedHosts: ['selabpi',],
     proxy: {
       '/api': {
         target: process.env.VITE_DEV_PROXY_TARGET || 'http://localhost:8080',
